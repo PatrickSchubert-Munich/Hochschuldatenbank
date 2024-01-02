8780673fc6385d8c98959f4056fb56dc3744c193
CODING-MONKEY\Patri <patrick.schubert.1@gmx.de>
2024-01-02 15:09:04 +0100

    Show Loading Screen

--- Hochschuldatenbank/Program.cs
+++ Hochschuldatenbank/Program.cs
@@ -11,7 +11,7 @@ namespace Hochschuldatenbank
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new LoadingScreen());
+            Application.Run(new LoadingScreen());
             Application.Run(new Hochschulverwaltung());
         }
     }
